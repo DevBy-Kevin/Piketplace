854ea010a9f5901d858d5d02542175bfbee94ac2
kevin <kowouvikossi3335@gmail.com>
2025-10-14 19:23:25 +0000

    Mise à jour responsive sur phone - menu de navigation

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/**
+ * Système de révélation des éléments au scroll
+ */
 const ratio = .1;
 const options = {
     root: null, //L'élément qui est utilisé comme zone d'affichage au moment d'évaluer la visibilité de la cible. Il doit être un ancêtre de la cible. S'il n'est pas spécifié ou s'il prend la valeur null, sa valeur par défaut est la zone d'affichage (le viewport) du navigateur.
@@ -19,3 +22,33 @@ const observer = new IntersectionObserver(handleIntersect, options)
 document.querySelectorAll('.reveal').forEach(function (r) { //Le function (r) s'appelle une fonction anonyme, tu pourras utilisé une fonction flèché à lea place aussi;
     observer.observe(r);
 })
+
+/**
+ * Menuu de navigation sur phone
+ */
+document.getElementById('menu-toggle')
+        .addEventListener('click', (event) => {
+            event.currentTarget.classList.toggle('open');
+            document.querySelector('.nav-topbar')
+                    .classList.toggle('open');
+        });
+
+
+/**
+ * Systèmes d'affichage d'informations suppléméntaires
+ */
+// Système d'affichage d'informations supplémentaires
+document.querySelectorAll('.list-item').forEach(item => {
+    item.addEventListener('click', event => {
+        const parent = event.currentTarget.parentElement;
+        // Retirer 'list-active' de tous les frères sauf celui cliqué
+        parent.querySelectorAll('.list-item.list-active').forEach(activeItem => {
+            if (activeItem !== event.currentTarget) {
+                activeItem.classList.remove('list-active');
+            }
+        });
+        // Basculer 'list-active' sur l'élément cliqué
+        event.currentTarget.classList.toggle('list-active');
+        event.currentTarget.querySelector('.js-cache').classList.toggle('list-active');
+    });
+});
